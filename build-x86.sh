c7d259cad0729acd0eb5f85d2bfe4d54c706f733
huypk <huy.phan@dirox.net>
2019-11-22 13:48:10 +0700

    Update build

--- build-x86.sh
+++
@@ -1,2 +0,0 @@
-cd openh264-master
-make OS=android NDKROOT=${ndk_root} ARCH=x86 TARGET=android-28 NDKLEVEL=16 
--- build-x86_64.sh
+++
@@ -1,2 +0,0 @@
-cd openh264-master
-make OS=android NDKROOT=${ndk_root} ARCH=x86_64 TARGET=android-28 NDKLEVEL=21
